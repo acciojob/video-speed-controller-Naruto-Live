364a95c5d5746a85b24d964533c821d1cc2ecb92
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-06-23 22:18:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.viewer');
+const toggle = document.querySelector('.toggle');
+const progressFilled = document.querySelector('.progress__filled');
+const progress = document.querySelector('.progress');
+const volume = document.querySelector('.volume');
+const playbackSpeed = document.querySelector('.playbackSpeed');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateButton() {
+  toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressFilled.style.width = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime =
+    (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function handleVolume() {
+  video.volume = this.value;
+}
+
+function handleSpeed() {
+  video.playbackRate = this.value;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
+
+volume.addEventListener('input', handleVolume);
+playbackSpeed.addEventListener('input', handleSpeed);
+
+skipButtons.forEach(button =>
+  button.addEventListener('click', skip)
+);
+
+progress.addEventListener('click', scrub);
